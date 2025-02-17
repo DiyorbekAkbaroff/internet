c1195015a02c51206b6c6817354c63936e5c07d5
Diyorbek <diorkooo041@gmail.com>
2025-02-17 13:09:06 +0500

    done.

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
 import socket
-import json
+import mysql.connector
+
+with open("domains.txt", "r") as file:
+    domains = file.readlines()
+
+db_config = {
+    'host': 'localhost',  
+    'user': 'root',
+    'password': '1111',
+    'database': 'Internet',
+    'collation': "utf8mb4_general_ci"
+}
+
+def connect_to_db():
+    connection = mysql.connector.connect(**db_config)
+    return connection
 
 
-with open('domains.txt') as f:
-    domains_list = f.read().split()
+def get_ip(domain):
+    ip = socket.gethostbyname(domain)
+    return ip
 
-    domains_json = []
-    for domain in domains_list:
-        domains_json.append({
-            "name": domain,
-            "ip": socket.gethostbyname(domain)
-        })
 
-with open('domains.json', 'w') as f:
-    f.write(json.dumps(domains_json, indent=4))
+def insert_domain_to_db(domain, ip):
+    connection = connect_to_db()
+    if connection is None:
+        print("MySQL server bilan bog'lanish muammosi!")
+        return
+
+    cursor = connection.cursor()
+    
+    cursor.execute("INSERT INTO Domain (domain, ip) VALUES (%s, %s)", (domain, ip))
+    connection.commit()
+    print(f"{domain} - {ip} muvaffaqiyatli qo'shildi.")
+
+    cursor.close()
+    connection.close()
